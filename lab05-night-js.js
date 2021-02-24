f304d473fad96c494245d979dbc1888c1faa5921
Kittinan Samakpong <joker.use.3@gmail.com>
2021-02-24 23:08:18 +0700

    full

--- lab05-night-js.js
+++ lab05-night-js.js
@@ -1,26 +1,66 @@
-function mode(event){
-    const greeting = event.currentTarget.querySelector(".Hi-day");
-    const day =  event.currentTarget.querySelector(".daytime");
-    const textcolor = event.currentTarget.querySelectorAll(".atDay");
-    const word = event.currentTarget.querySelector("#text-day");
-    event.currentTarget.classList.remove('day');
-    event.currentTarget.classList.add('night');
+function mode(){
+    const Onclick = document.querySelector("#time");
+    const greeting = Onclick.querySelector("#Hi-day");
+    const day =  Onclick.querySelector("#thistime");
+    const textcolor = Onclick.querySelectorAll(".p");
+    const textcolor2 = Onclick.querySelector("#Hi-day");
+    const textcolor3 = Onclick.querySelector("#thistime");
+    const word = Onclick.querySelector("#text");
+    const Active = Onclick.querySelector("#button");
+    const text2 = Onclick.querySelector(".text2");
+    if(onClick === true){
+        Onclick.classList.remove('day');
+        Onclick.classList.add('night');
+        greeting.innerHTML = "GOOD NIGHT";
+        day.innerHTML="This is night time!";
 
-    greeting.innerHTML = "GOOD NIGHT";
-    day.innerHTML="This is night time!";
+        for(var i=0;i<textcolor.length;i++){
+            textcolor[i].classList.remove('atDay');
+            textcolor[i].classList.add('atNight');    
+        }
 
-    for(var i=0;i<textcolor.length;i++){
-        textcolor[i].classList.remove('atDay');
-        textcolor[i].classList.add('atNight');    
+        textcolor2.classList.remove('atDay');
+        textcolor2.classList.add('atNight');
+
+        textcolor3.classList.remove('atDay');
+        textcolor3.classList.add('atNight');
+        
+        Active.innerHTML="Active the day mode";
+
+        word.classList.remove('text-day');
+        word.classList.add('text-night');
+
+        text2.innerHTML = 'day';
+        onClick = false;
+
     }
-    word.classList.remove('text-day');
-    word.classList.add('text-night');
 
-    
+    else{
 
+        Active.innerHTML="Active the night mode";
 
-    console.log(day);
-    
+        Onclick.classList.remove('night');
+        Onclick.classList.add('day');
+
+        greeting.innerHTML = "GOOD MORNING";
+        day.innerHTML="This is day time!";
+
+        for(var i=0;i<textcolor.length;i++){
+            textcolor[i].classList.remove('atNight');    
+            textcolor[i].classList.add('atDay');
+        }
+        word.classList.remove('text-night');
+        word.classList.add('text-day');
+
+        textcolor2.classList.remove('atNight');
+        textcolor2.classList.add('atDay');
+
+        textcolor3.classList.remove('atNight');
+        textcolor3.classList.add('atDay');
+        text2.innerHTML = 'night';
+        onClick = true;
+    }
 }
+var onClick = true ;
 const button = document.querySelector("#time");
 button.addEventListener('click',mode);
