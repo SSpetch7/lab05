d0ff308105da0cc42e9fe572bd67d5bf3605a109
Kittinan Samakpong <joker.use.3@gmail.com>
2021-02-24 21:07:14 +0700

    night

--- lab05-night-js.js
+++ lab05-night-js.js
@@ -1,6 +1,26 @@
 function mode(event){
-    const day = event.currentTarget.querySelector('.Hi-day');
-    day.textContent('GOOD NIGHT');
+    const greeting = event.currentTarget.querySelector(".Hi-day");
+    const day =  event.currentTarget.querySelector(".daytime");
+    const textcolor = event.currentTarget.querySelectorAll(".atDay");
+    const word = event.currentTarget.querySelector("#text-day");
+    event.currentTarget.classList.remove('day');
+    event.currentTarget.classList.add('night');
+
+    greeting.innerHTML = "GOOD NIGHT";
+    day.innerHTML="This is night time!";
+
+    for(var i=0;i<textcolor.length;i++){
+        textcolor[i].classList.remove('atDay');
+        textcolor[i].classList.add('atNight');    
+    }
+    word.classList.remove('text-day');
+    word.classList.add('text-night');
+
+    
+
+
+    console.log(day);
+    
 }
-const button = document.querySelector('body');
+const button = document.querySelector("#time");
 button.addEventListener('click',mode);
