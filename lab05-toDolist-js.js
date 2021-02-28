6ffdef5ec5cd34103d54c3f539dd3b9650e7acbb
Kittinan Samakpong <joker.use.3@gmail.com>
2021-02-28 19:18:51 +0700

    test

--- lab05-toDolist-js.js
+++ lab05-toDolist-js.js
@@ -1,8 +1,37 @@
+var mylist = document.getElementById('li');
+var i; 
+for (i=0;i < mylist.length;i++){
+    var trash = document.createElement('i');
+    var text = document.createTextNode('\u007D');
+    trash.className ='fas fa-trash delete';
+    trash.appendChild(text);
+    mylist[i].appendChild(trash);
+}
+var remove = document.getElementsByClassName('delete');
+var i;
+for(i=0;i<remove.length;i++){
+    remove[i].onclick =function(){
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+var list = document.querySelector('#list');
+list.addEventListener('click',function(event){
+    if(event.target.tagName === 'li'){
+        event.target.classList.toggle('checked');
+    }
+},false)
+
+function newNode(){
+    var li = document.createElement('li');
+    var inputValue = document.getElementById('input')
+}
 const list = document.getElementById('list');
 const todo = document.getElementById('input');
 todo.addEventListener('keyup',function(event){
     if(event.keyCode == 13){
         const toDo = input.value;
-        if(toDo)
+        
     }
 })
