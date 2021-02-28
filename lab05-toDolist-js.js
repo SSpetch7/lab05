10c04d278776e758f106481a68c9fe97e6a61d17
Kittinan Samakpong <joker.use.3@gmail.com>
2021-02-28 16:54:19 +0700

    new

--- lab05-toDolist-js.js
+++ lab05-toDolist-js.js
@@ -1,10 +1,8 @@
-function addlist(){
-    const text= <li class='item'>
-
-                </li>
-}
-
-
-const input = document.getElementById('input');
-
 const list = document.getElementById('list');
+const todo = document.getElementById('input');
+todo.addEventListener('keyup',function(event){
+    if(event.keyCode == 13){
+        const toDo = input.value;
+        if(toDo)
+    }
+})
